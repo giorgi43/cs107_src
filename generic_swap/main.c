d992037c0646b1b2b0a9cd040710cb0869d755c1
urandom <urandom43@gmail.com>
2020-10-06 21:14:06 +0400

    add string test

--- generic_swap/main.c
+++ generic_swap/main.c
@@ -5,9 +5,16 @@ int main(int argc, char** argv) {
 	/* swap ints */
 	int a = 5;
 	int b = 44;
-	printf("before swapping: a = %d, b = %d\n", a, b);
+	printf("before swapping: a = %d b = %d\n", a, b);
 	swap(&a, &b, sizeof(int));
-	printf("after swapping: a = %d, b = %d\n", a, b);
+	printf("after swapping: a = %d b = %d\n", a, b);
+
+	/* swap strings */
+	char* str1 = "byte beat";
+	char* str2 = "Bigfoot";
+	printf("before swapping: str1 = \"%s\" str2 = \"%s\"\n", str1, str2);
+	swap(&str1, &str2, sizeof(char*));	
+	printf("after swapping: str1 = \"%s\" str2 = \"%s\"\n", str1, str2);
 
 	return 0;
 }
